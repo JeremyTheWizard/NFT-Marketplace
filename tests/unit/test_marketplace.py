a254041324f69d7df9a684cf55ef2b98a054f6d4
Mijail <piekarzmijail@gmail.com>
2022-05-13 20:32:21 -0300

    added new erc721 token and modified test_marketplace.py to include it

--- tests/unit/test_marketplace.py
+++ tests/unit/test_marketplace.py
@@ -1,5 +1,14 @@
 import pytest
-from brownie import network, Marketplace, TestToken, chain, Wei, accounts
+from brownie import (
+    network,
+    Marketplace,
+    TestToken,
+    TestToken2,
+    chain,
+    Wei,
+    accounts,
+    exceptions,
+)
 from scripts.deploy import deploy
 from scripts.helpful_scripts import (
     get_account,
@@ -19,25 +28,30 @@ def get_account_and_deploy():
     return account, marketplace
 
 
-testToken = None
+test_tokens = []
 
 
 @pytest.fixture(scope="module")
-def deploy_erc721():
-    global testToken
-    testToken = TestToken.deploy({"from": account})
+def deploy_test_tokens():
+    global test_tokens
+    test_tokens = [
+        TestToken.deploy({"from": account}),
+        TestToken2.deploy({"from": accounts[1]}),
+    ]
 
 
-def test_add_nft(get_account_and_deploy, deploy_erc721):
-    testToken.approve(marketplace.address, 0)
-    marketplace.addNft(testToken, 0, Wei("0.0001 ether"))
+def test_add_nft(get_account_and_deploy, deploy_test_tokens):
+    test_tokens[0].approve(marketplace.address, 0)
+    marketplace.addNft(test_tokens[0], 0, Wei("0.0001 ether"))
     assert marketplace.getNftToOwner(0) == account.address
 
 
-def test_buy_nft(get_account_and_deploy, deploy_erc721):
+def test_buy_nft(get_account_and_deploy, deploy_test_tokens):
     print(f"Account0 = {account}\nAccount1 = {accounts[1]}")
     print(f"Current owner = {account}")
     new_owner = accounts[1]
-    marketplace.buyNft(testToken, 0, account, new_owner, {"value": Wei("0.0001 ether")})
+    marketplace.buyNft(
+        test_tokens[0], 0, account, new_owner, {"value": Wei("0.0001 ether")}
+    )
     print(f"New owner = {marketplace.getNftToOwner(0)}")
     assert marketplace.getNftToOwner(0) == new_owner
