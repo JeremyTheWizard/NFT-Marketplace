84d64ff5f11fc04a62528150c2fd4fbe995681e4
Mijail Piekarz <piekarzmijail@gmail.com>
2022-05-17 09:28:42 -0300

    Added custom configuration to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0F1D2B",
+        "button-primary": "#37EF97",
+        "button-secondary": "1D9DF9",
+      },
+    },
+    backdropFilter: {
+      none: "none",
+      blur: "blur(20px)",
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-filters")],
 };
