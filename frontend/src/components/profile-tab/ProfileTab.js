58696fbe3a63099d7bea97b2a718667d4b1559dd
Mijail Piekarz <piekarzmijail@gmail.com>
2022-05-31 20:24:32 -0300

    Now the items tab displays the current account owned NFTs

--- frontend/src/components/profile-tab/ProfileTab.js
+++ frontend/src/components/profile-tab/ProfileTab.js
@@ -1,11 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useMoralisWeb3Api } from "react-moralis";
+import { useEthers } from "@usedapp/core";
+import CollectionCard from "../items-activity/CollectionCard";
+import randomPerson from "../../photos/random-person.jpeg";
 
-function ItemsActivity() {
+function ProfileTab() {
   const [itemsDisplay, setItemsDisplay] = useState("flex");
   const [offersMadeDisplay, setOffersMadeDisplay] = useState("hidden");
   const [offersReceivedDisplay, setOffersReceivedDisplay] = useState("hidden");
   const [collectionsCreatedDisplay, setCollectionsCreatedDisplay] =
     useState("hidden");
+  const [userNFTs, setUserNFTs] = useState();
+  const [nftsCardsHtml, setNftsCardsHtml] = useState();
+  const Web3Api = useMoralisWeb3Api();
+  const { account } = useEthers();
+
+  const fetchNFTs = async () => {
+    const userNFTss = await Web3Api.Web3API.account.getNFTs({
+      chain: "rinkeby",
+      address: account,
+    });
+    setUserNFTs(userNFTss);
+  };
+
+  useEffect(() => {
+    account && fetchNFTs();
+  }, [account]);
+
+  useEffect(() => {
+    userNFTs && renderExploreCards();
+  }, [userNFTs]);
+
+  const fixMoralisUrl = (url) => {
+    if (url.startsWith("ipfs")) {
+      return "https://ipfs:moralis.io:2053/ipfs/" + url.split("ipfs://ipfs/");
+    } else {
+      return url + "?format=json";
+    }
+  };
+
+  const fixUrl = (url) => {
+    return "https://ipfs.io/ipfs/" + url.split("ipfs://")[1];
+  };
+
+  async function renderExploreCards() {
+    const nftsCardsHtml = [];
+    const requests = userNFTs.result.map(async (nft) => {
+      let response = await fetch(fixMoralisUrl(nft.token_uri));
+      response = await response.json();
+      const image = response.image;
+      nftsCardsHtml.push(
+        <CollectionCard
+          imagePath={image.startsWith("ipfs") ? fixUrl(image) : image}
+          owner={""}
+        />
+      );
+    });
+    await Promise.all(requests);
+    setNftsCardsHtml(nftsCardsHtml);
+  }
 
   return (
     <div className="w-full my-12">
@@ -46,7 +99,9 @@ function ItemsActivity() {
       <hr className="bg-onPrimary w-full border-1" />
       <div
         className={`${itemsDisplay} mt-12 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-12`}
-      ></div>
+      >
+        {nftsCardsHtml}
+      </div>
       <div
         className={`${
           itemsDisplay === "flex" ? "hidden" : "flex"
@@ -56,4 +111,4 @@ function ItemsActivity() {
   );
 }
 
-export default ItemsActivity;
+export default ProfileTab;
