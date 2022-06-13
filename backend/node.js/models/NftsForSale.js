57029414217bb06c93db1835f6cb53eb85a02058
Mijail Piekarz <piekarzmijail@gmail.com>
2022-06-13 12:40:37 -0300

    Modified NftsForSale schema to include original owner

--- backend/node.js/models/NftsForSale.js
+++ backend/node.js/models/NftsForSale.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
-const nftsForSaleSchema = mongoose.Schema(
-  {
-    contract: { type: String, required: true },
-    tokenId: { type: String, required: true },
-  }
-  //{ timestamps: true }
-);
+const nftsForSaleSchema = mongoose.Schema({
+  contract: { type: String, required: true },
+  tokenId: { type: String, required: true },
+  owner: { type: String, required: true },
+});
 
 export default mongoose.models.NftsForSale ||
   mongoose.model("NftsForSale", nftsForSaleSchema);
