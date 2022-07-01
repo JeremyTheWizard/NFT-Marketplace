17a4247a3320fae0b11c2ffe6638e2a6946d72db
Mijail Piekarz <piekarzmijail@gmail.com>
2022-07-01 11:49:10 -0300

    Added a bottom margin to the Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,9 @@
-import TopCollections from "../components/top-collections/TopCollections";
 import Explore from "../components/explore/Explore";
+import TopCollections from "../components/top-collections/TopCollections";
 
 function Home() {
   return (
-    <div className="w-[90vw] max-w-[1200px] mx-auto">
+    <div className="w-[90vw] max-w-[1200px] mx-auto mb-12">
       <TopCollections />
       <Explore />
     </div>
