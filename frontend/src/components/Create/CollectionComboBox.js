8119edf9d5e37dc5c91144616105550ccb18a1eb
Mijail Piekarz <piekarzmijail@gmail.com>
2022-07-05 10:37:31 -0300

    The CollectionComboBox now fetches the user created collection from the mongo db and shows a loading icon while doing so

--- frontend/src/components/Create/CollectionComboBox.js
+++ frontend/src/components/Create/CollectionComboBox.js
@@ -1,5 +1,8 @@
 import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
-import * as React from "react";
+import { useEthers } from "@usedapp/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ModifiedCircularProgress from "../ModifiedMuiComponents/ModifiedCircularProgress";
 import ModifiedTextField from "../ModifiedTextField";
 import CreateCollectionDialog from "./CreateCollectionDialog";
 
@@ -7,11 +10,56 @@ const filter = createFilterOptions();
 
 const CollectionComboBox = () => {
   const [value, setValue] = React.useState(null);
-  const [open, toggleOpen] = React.useState(false);
+  const [openCreateCollectionDialog, setOpenCreateCollectionDialog] =
+    useState(false);
+  const [openAutocompleteOptions, setOpenAutocompleteOptions] = useState(false);
+  const [userCollections, setUserCollections] = useState();
+  const { account } = useEthers();
+  const loading = openAutocompleteOptions && !userCollections;
+
+  const updateUserCollections = async () => {
+    const response = await axios.get(
+      `http://localhost:8000/api/users/user/${account}`
+    );
+    setUserCollections(response.data.user.collectionsCreated);
+  };
+
+  useEffect(() => {
+    if (account) {
+      let active = true;
+
+      if (!loading) {
+        return undefined;
+      }
+      (async () => {
+        if (active) {
+          await updateUserCollections();
+        }
+      })();
+
+      return () => {
+        active = false;
+      };
+    }
+  }, [account, loading]);
+
+  useEffect(() => {
+    if (!openAutocompleteOptions) {
+      setUserCollections(undefined);
+    }
+  }, [openAutocompleteOptions]);
 
   return (
     <>
       <Autocomplete
+        open={openAutocompleteOptions}
+        onOpen={() => {
+          setOpenAutocompleteOptions(true);
+        }}
+        onClose={() => {
+          setOpenAutocompleteOptions(false);
+        }}
+        loading={loading}
         value={value}
         ListboxProps={{
           sx: { "& li:first-child": { fontWeight: "bold" } },
@@ -21,18 +69,18 @@ const CollectionComboBox = () => {
         onChange={(_, newValue) => {
           if (
             newValue &&
-            newValue.title &&
-            newValue.title === "+ Create new collection"
+            newValue.name &&
+            newValue.name === "+ Create new collection"
           ) {
-            toggleOpen(true);
+            setOpenCreateCollectionDialog(true);
           } else if (typeof newValue === "string") {
             setValue({
-              title: newValue,
+              name: newValue,
             });
           } else if (newValue && newValue.inputValue) {
             // Create a new value from the user input
             setValue({
-              title: newValue.inputValue,
+              name: newValue.inputValue,
             });
           } else {
             setValue(newValue);
@@ -44,22 +92,22 @@ const CollectionComboBox = () => {
           const { inputValue } = params;
           // Suggest the creation of a new value
           const isExisting = options.some(
-            (option) => inputValue === option.title
+            (option) => inputValue === option.name
           );
           if (filtered.length === 0) {
             filtered.shift();
             filtered.push({
               inputValue,
-              title: `Create collection "${inputValue}"`,
+              name: `Create collection "${inputValue}"`,
               description:
                 "This is a quick creation. You'll be able to fill the collection's details later on your profile.",
             });
-            return filtered;
+            return loading ? [] : filtered;
           }
 
           return [
             {
-              title: "+ Create new collection",
+              name: "+ Create new collection",
               style: { fontWeight: "bold" },
             },
             ...filtered,
@@ -69,7 +117,7 @@ const CollectionComboBox = () => {
         clearOnBlur
         handleHomeEndKeys
         id="collection-autocomplete"
-        options={top100Films}
+        options={userCollections ? userCollections : []}
         getOptionLabel={(option) => {
           // Value selected with enter, right from the input
           if (typeof option === "string") {
@@ -80,7 +128,7 @@ const CollectionComboBox = () => {
             return option.inputValue;
           }
           // Regular option
-          return option.title;
+          return option.name;
         }}
         renderOption={(props, option) => {
           return (
@@ -93,7 +141,7 @@ const CollectionComboBox = () => {
                 alignItems: "flex-start",
               }}
             >
-              <p>{option.title}</p>
+              <p>{option.name}</p>
               <p className="text-sm font-normal">{option.description}</p>
             </li>
           );
@@ -105,145 +153,25 @@ const CollectionComboBox = () => {
             label="Collection"
             name="collection"
             id="collection"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <React.Fragment>
+                  {loading ? <ModifiedCircularProgress size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </React.Fragment>
+              ),
+            }}
           />
         )}
       />
       <CreateCollectionDialog
         setValue={setValue}
-        open={open}
-        toggleOpen={toggleOpen}
+        open={openCreateCollectionDialog}
+        toggleOpen={setOpenCreateCollectionDialog}
       />
     </>
   );
 };
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-  {
-    title: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-  { title: "The Good, the Bad and the Ugly", year: 1966 },
-  { title: "Fight Club", year: 1999 },
-  {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-  },
-  {
-    title: "Star Wars: Episode V - The Empire Strikes Back",
-    year: 1980,
-  },
-  { title: "Forrest Gump", year: 1994 },
-  { title: "Inception", year: 2010 },
-  {
-    title: "The Lord of the Rings: The Two Towers",
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: "Goodfellas", year: 1990 },
-  { title: "The Matrix", year: 1999 },
-  { title: "Seven Samurai", year: 1954 },
-  {
-    title: "Star Wars: Episode IV - A New Hope",
-    year: 1977,
-  },
-  { title: "City of God", year: 2002 },
-  { title: "Se7en", year: 1995 },
-  { title: "The Silence of the Lambs", year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: "Life Is Beautiful", year: 1997 },
-  { title: "The Usual Suspects", year: 1995 },
-  { title: "Léon: The Professional", year: 1994 },
-  { title: "Spirited Away", year: 2001 },
-  { title: "Saving Private Ryan", year: 1998 },
-  { title: "Once Upon a Time in the West", year: 1968 },
-  { title: "American History X", year: 1998 },
-  { title: "Interstellar", year: 2014 },
-  { title: "Casablanca", year: 1942 },
-  { title: "City Lights", year: 1931 },
-  { title: "Psycho", year: 1960 },
-  { title: "The Green Mile", year: 1999 },
-  { title: "The Intouchables", year: 2011 },
-  { title: "Modern Times", year: 1936 },
-  { title: "Raiders of the Lost Ark", year: 1981 },
-  { title: "Rear Window", year: 1954 },
-  { title: "The Pianist", year: 2002 },
-  { title: "The Departed", year: 2006 },
-  { title: "Terminator 2: Judgment Day", year: 1991 },
-  { title: "Back to the Future", year: 1985 },
-  { title: "Whiplash", year: 2014 },
-  { title: "Gladiator", year: 2000 },
-  { title: "Memento", year: 2000 },
-  { title: "The Prestige", year: 2006 },
-  { title: "The Lion King", year: 1994 },
-  { title: "Apocalypse Now", year: 1979 },
-  { title: "Alien", year: 1979 },
-  { title: "Sunset Boulevard", year: 1950 },
-  {
-    title:
-      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-    year: 1964,
-  },
-  { title: "The Great Dictator", year: 1940 },
-  { title: "Cinema Paradiso", year: 1988 },
-  { title: "The Lives of Others", year: 2006 },
-  { title: "Grave of the Fireflies", year: 1988 },
-  { title: "Paths of Glory", year: 1957 },
-  { title: "Django Unchained", year: 2012 },
-  { title: "The Shining", year: 1980 },
-  { title: "WALL·E", year: 2008 },
-  { title: "American Beauty", year: 1999 },
-  { title: "The Dark Knight Rises", year: 2012 },
-  { title: "Princess Mononoke", year: 1997 },
-  { title: "Aliens", year: 1986 },
-  { title: "Oldboy", year: 2003 },
-  { title: "Once Upon a Time in America", year: 1984 },
-  { title: "Witness for the Prosecution", year: 1957 },
-  { title: "Das Boot", year: 1981 },
-  { title: "Citizen Kane", year: 1941 },
-  { title: "North by Northwest", year: 1959 },
-  { title: "Vertigo", year: 1958 },
-  {
-    title: "Star Wars: Episode VI - Return of the Jedi",
-    year: 1983,
-  },
-  { title: "Reservoir Dogs", year: 1992 },
-  { title: "Braveheart", year: 1995 },
-  { title: "M", year: 1931 },
-  { title: "Requiem for a Dream", year: 2000 },
-  { title: "Amélie", year: 2001 },
-  { title: "A Clockwork Orange", year: 1971 },
-  { title: "Like Stars on Earth", year: 2007 },
-  { title: "Taxi Driver", year: 1976 },
-  { title: "Lawrence of Arabia", year: 1962 },
-  { title: "Double Indemnity", year: 1944 },
-  {
-    title: "Eternal Sunshine of the Spotless Mind",
-    year: 2004,
-  },
-  { title: "Amadeus", year: 1984 },
-  { title: "To Kill a Mockingbird", year: 1962 },
-  { title: "Toy Story 3", year: 2010 },
-  { title: "Logan", year: 2017 },
-  { title: "Full Metal Jacket", year: 1987 },
-  { title: "Dangal", year: 2016 },
-  { title: "The Sting", year: 1973 },
-  { title: "2001: A Space Odyssey", year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: "Toy Story", year: 1995 },
-  { title: "Bicycle Thieves", year: 1948 },
-  { title: "The Kid", year: 1921 },
-  { title: "Inglourious Basterds", year: 2009 },
-  { title: "Snatch", year: 2000 },
-  { title: "3 Idiots", year: 2009 },
-  { title: "Monty Python and the Holy Grail", year: 1975 },
-];
 
 export default CollectionComboBox;
